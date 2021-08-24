8781aaa8f2d1a624b7db39bad9e79ca3703ac90b
Ronald Mosomi <ronaldmosomi@gmail.com>
2021-08-24 19:05:07 +0300

    testing to check

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
+from plotly import graph_objects as go
 
-st.write(" " "
-#My app
-Hello world
-"
-"
-"
-)
+st.title("My chart")
 
-df= pd.read_csv("df_crops.csv")
-st.line_chart(df)
+df = pd.read_csv("df_crops.csv").set_index("Produce_Variety")
+
+st.bar_chart(df)
